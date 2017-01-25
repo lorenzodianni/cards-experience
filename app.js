c29a2e633be5a4e4567791d70d588645d5e65813
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2017-01-25 02:25:23 +0100

    transition in

--- app.js
+++ app.js
@@ -1,91 +1,151 @@
-const Card = (options) => {
-  return `
-  <div class="card">
-    <div class="card__header" style="background-image: url(${options.image})">
-      <div class="profile__picture" style="background-image: url(${options.logo})"></div>
-    </div>
-    <div class="card__body">
-      <div class="card__info">
-        <span class="card__info-name">${options.name}</span>
-        <div class="card__info-projects">${options.projects}</div>
+const ProfilePicture = (imageUrl) => {
+  return `<div class="profile-picture" style="background-image: url(${imageUrl})"></div>`
+}
+
+const BannerImage = (imageUrl, {child}) => {
+  return `<div class="banner-image" style="background-image: url(${imageUrl})">${child}</div>`
+}
+
+class ViewCard {
+  static create(options) {
+    let wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+    <div class="view-card">
+      ${BannerImage(options.image, {
+        child: `
+          ${ProfilePicture(options.logo)}
+          <div class="view-card__close"></div>
+          <div class="view-card__presentation">
+          <div class="view-card__title">${options.title}</div>
+          <div class="view-card__subtitle">${options.subtitle}</div>
+          </div>
+        `
+      })}
+      <div class="view-card__body">
+        ${Card.create({image: options.image, title: 'Project X', subtitle: '32 screens'}).outerHTML}
       </div>
-      <div class="card__users">
-        ${options.users.map((userImg, i) => {
-          if(i <= 2) {
-            let attr = i === 2 ? `more-users="+${options.users.length - (i+1)}"` : ''
-            return `<div class="card__user" style="background-image: url(${userImg})" ${attr}></div>`;
-          }
-        }).join('')}
+    </div>
+    `
+    let _el = wrapper.children[0];
+    _el.querySelector('.view-card__close').addEventListener('click', ViewCard.close.bind(_el));
+    return _el;
+  }
+
+  static close() {
+    this.remove();
+  }
+}
+
+
+class Card {
+  static create(options) {
+    let wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+    <div class="card">
+      ${BannerImage(options.image, {
+        child: options.logo ? ProfilePicture(options.logo) : ''
+      })}
+      <div class="card__body">
+        <div class="card__info">
+          <span class="card__info-title">${options.title}</span>
+          <div class="card__info-subtitle">${options.subtitle}</div>
+        </div>
+        <div class="card__specials">
+          ${options.specials ? options.specials.map((specialImg, i) => {
+            if (i <= 2) {
+              let attr = i === 2 ? `more-specials="+${options.specials.length - (i + 1)}"` : ''
+              return `<div class="card__special" style="background-image: url(${specialImg})" ${attr}></div>`;
+            }
+          }).join('') : ''}
+        </div>
       </div>
     </div>
-  </div>
-  `
+    `
+    let _el = wrapper.children[0];
+    _el.card = Object.assign({}, options);
+    _el.applyParallax = Card.applyParallax.bind(null, _el, '.banner-image');
+    return _el;
+  }
+
+  static applyParallax(element, section) {
+    let {left, width} = element.getBoundingClientRect();
+    let position = 100 / (document.body.clientWidth / (left + (width / 2)));
+    element.querySelector(section)
+      .style.backgroundPosition = `${position}% center`;
+  }
+
+  static clone(element, className) {
+    let _elClientRect = element.getBoundingClientRect();
+    let _elClone = element.cloneNode(true);
+    _elClone.card = Object.assign({}, element.card);
+
+    className ? _elClone.classList.add(className) : null;
+    _elClone.style.top = `${_elClientRect.top}px`;
+    _elClone.style.left = `${_elClientRect.left}px`;
+    _elClone.style.height = `${_elClientRect.height}px`;
+    _elClone.style.width = `${_elClientRect.width}px`;
+    return _elClone
+  }
+
+  static bindTransition(card, callbackTransitionEnd) {
+    let _clone = Card.clone(card, 'card--clone');
+    document.body.appendChild(_clone);
+    _clone.addEventListener('transitionend', callbackTransitionEnd);
+    setTimeout(() => _clone.classList.add('card--clone__moving'), 50);
+  }
 }
 
 const forEach = (array, fn) => Array.from(array).forEach(fn);
-const applyParallax = (element, i) => {
-  let {left, width} = element.getBoundingClientRect();
-  let position = 100 / (document.body.clientWidth / (left + (width/2)));
-  element.querySelector('.card__header')
-    .style.backgroundPosition = `${position}% center`;
-}
+const bindParallax = (nodeChildren) => forEach(nodeChildren, card => card.applyParallax());
 
-let cards = [1,2,3].reduce(acc => {
+let cards = [1, 2, 3].reduce(acc => {
   acc = acc.concat([
-    Card({
-      image: './img/seattle.png',
-      name: 'Nike Running',
-      projects: '14 PROJECTS',
-      logo: './img/seattle.png',
-      users: ['./img/userA.jpg', './img/userB.jpg', './img/userC.png', 3,4,5]
-    }),
-    Card({
-      image: './img/anduin.png',
-      name: 'Relate UI Kit',
-      projects: '7 PROJECTS',
-      logo: './img/anduin.png',
-      users: ['./img/userD.png', './img/userE.png',  './img/userF.jpg', 4,5,6,7,8]
-    }),
-    Card({
+    Card.create({
       image: './img/route-66.png',
-      name: 'InVision Craft',
-      projects: '3 PROJECTS',
+      title: 'InVision Craft',
+      subtitle: '3 PROJECTS',
       logo: './img/route-66.png',
-      users: ['./img/userA.jpg', './img/userB.jpg']
+      specials: ['./img/userA.jpg', './img/userB.jpg']
     }),
-    Card({
+    Card.create({
+      image: './img/seattle.png',
+      title: 'Nike Running',
+      subtitle: '14 PROJECTS',
+      logo: './img/seattle.png',
+      specials: ['./img/userA.jpg', './img/userB.jpg', './img/userC.png', 3, 4, 5]
+    }),
+    Card.create({
+      image: './img/anduin.png',
+      title: 'Relate UI Kit',
+      subtitle: '7 PROJECTS',
+      logo: './img/anduin.png',
+      specials: ['./img/userD.png', './img/userE.png', './img/userF.jpg', 4, 5, 6, 7, 8]
+    }),
+    Card.create({
       image: './img/sunset.png',
-      name: 'Serum Design',
-      projects: '18 PROJECTS',
+      title: 'Serum Design',
+      subtitle: '18 PROJECTS',
       logo: './img/sunset.png',
-      users: ['./img/userC.png', './img/userD.png', './img/userE.png', 4,5,6,7,8,9,10,11,12]
+      specials: ['./img/userC.png', './img/userD.png', './img/userE.png', 4, 5, 6, 7, 8, 9, 10, 11, 12]
     })
   ]);
   return acc;
-}, [])
+}, []);
 
 const cardsDOM = document.querySelector('.cards');
-cardsDOM.innerHTML = cards.join('');
-document.addEventListener('DOMContentLoaded', () => {
-  forEach(cardsDOM.children, applyParallax);
-})
-cardsDOM.addEventListener('scroll', e => {
-  forEach(e.target.children, applyParallax);
+forEach(cards, (card) => cardsDOM.appendChild(card));
+const cardsItems = cardsDOM.children;
+document.addEventListener('DOMContentLoaded', bindParallax.bind(null, cardsItems));
+cardsDOM.addEventListener('scroll', bindParallax.bind(null, cardsItems));
+
+forEach(cardsItems, card => {
+  card.addEventListener('click', () => Card.bindTransition(card, onTransitionEnd(0)));
 });
 
-forEach(cardsDOM.children, (card) => {
-  card.addEventListener('click', function() {
-    let profilePicture = this.querySelector('.profile__picture');
-    let cardHeader = this.querySelector('.card__header');
-
-    let _pClone = profilePicture.cloneNode(true);
-    _pClone.style.top = `${profilePicture.getBoundingClientRect().top}px`;
-    _pClone.style.left = `${profilePicture.getBoundingClientRect().left}px`;
-    _pClone.style.transform = `none`;
-    document.body.appendChild(_pClone);
-    console.log(profilePicture.getBoundingClientRect());
-    console.log(cardHeader.getBoundingClientRect());
-    console.dir(this)
-  });
-})
+const onTransitionEnd = (counter) => (e) => {
+  if (counter) return;
+  ++counter;
+  e.target.remove();
+  document.body.appendChild(ViewCard.create(Object.assign({}, e.target.card)));
+}
 
